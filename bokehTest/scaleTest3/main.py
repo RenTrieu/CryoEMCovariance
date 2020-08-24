246fbf904217f893b9e8337e4d66385ff53ad394
recovery1 <recovery1@Leeks>
2020-08-24 14:03:38 -0700

    minor edit

--- bokehTest/scaleTest3/main.py
+++ bokehTest/scaleTest3/main.py
@@ -2,12 +2,12 @@ from bokeh.models import FuncTickFormatter, ColumnDataSource
 from bokeh.plotting import figure
 from bokeh.util.compiler import TypeScript
 from bokeh.core.properties import Dict, ColumnData
+from bokeh.core.property.bases import Property
 
-string1 = 'var1'
+string1 = 'ColumnData'
 tickSource = ColumnDataSource({'var1' : [5]})
 
 fFormatter = FuncTickFormatter()
-fFormatter.args = Dict(string1, ColumnDataSource)
-
+fFormatter.args = Dict(string1, tickSource)
 
 
